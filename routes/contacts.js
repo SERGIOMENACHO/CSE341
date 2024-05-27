a495d397d94ddf2328cbec46944e8be45813642f
SERGIOMENACHO <82847158+SERGIOMENACHO@users.noreply.github.com>
2024-05-27 21:08:05 +0300

    W03 personal assignment

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,8 +1,14 @@
-const routes = require("express").Router()
-const contactsController = require("../controllers/contacts")
+const routes = require("express").Router();
+const contactsController = require("../controllers/contacts");
 
-routes.get("/contacts", contactsController.getAllContacts)
+routes.get("/contacts", contactsController.getAllContacts);
 
-routes.get("/contacts/:id", contactsController.getOneContact)
+routes.get("/contacts/:id", contactsController.getOneContact);
+
+routes.post("/contacts/create", contactsController.createContact);
+
+routes.put("/contacts/:id", contactsController.updateContact);
+
+routes.delete("/contacts/:id", contactsController.deleteContact);
 
 module.exports = routes;

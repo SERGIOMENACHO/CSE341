f6c7adcda873f1f4eed907f959a5ec4ea0ace49a
SERGIOMENACHO <82847158+SERGIOMENACHO@users.noreply.github.com>
2024-05-27 21:59:55 +0300

    cors issue

--- server.js
+++ server.js
@@ -19,6 +19,13 @@ mongoose
 app
   .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
   .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+  })
   .use("/", require("./routes"));
 
 app.listen(port, () => {
